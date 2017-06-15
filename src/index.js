6d19334be358e4ce72f3584f41923f1fd3e4cb39
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 19:33:18 -0400

    moving api key and git ignore

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-	
-const API_KEY ='AIzaSyABvY1iF_aNf63ICTi7KXk3gjKgutfIjzw';
+import { API_KEY } from '../config.js';
+
 
 // const App = () => {
 	// return (
